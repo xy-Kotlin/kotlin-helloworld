fd5317c9abd985a4fe9307f868ff75116e3ce995
winfuture <insoul_net@qq.com>
2020-02-09 23:53:45 +0800

    完成第一个Kotlin程序，kotlin可以使用jvm，也可以在缺少jvm的情况下编译成js

---
+++ README.md
@@ -0,0 +1,2 @@
+1. 功能
+实现使用kotlin打印helloworld以及其他数据类型
---
+++ src/helloworld.kt
@@ -0,0 +1,22 @@
+fun main(args:Array<String>){
+    var b:Byte=2
+    val c:Char='c'
+    var s:Short=2222
+    var i:Int=222
+    var f:Float=22222.22f
+    var d:Double=22222.222
+    var l:Long=2222222222222
+    var boolean:Boolean=false
+    var string:String="helloworld"
+    println(b)
+    println(c)
+    println(s)
+    println(i)
+    println(f)
+    println(d)
+    println(l)
+    println(boolean)
+    println(string+" "+string)
+
+
+}
